a8e525e232c6a8442c838980e420da5e265430c8
julitcho <julio.alvarezapolinario2@gmail.com>
2025-08-16 11:17:39 -0300

    modificación html y agregar css

---
+++ eventos.js
@@ -0,0 +1,10 @@
+function mostrarSaludo() {
+    alert('Hola!');
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const divContenedor = document.querySelector(".contenedor");
+
+    divContenedor.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
