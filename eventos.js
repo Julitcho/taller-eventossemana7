8d16bd98339fb17b37121ba4529b91fcf31c42f2
julitcho <julio.alvarezapolinario2@gmail.com>
2025-08-16 14:17:24 -0300

    evitar la propagacion del click

--- eventos.js
+++ eventos.js
@@ -1,10 +1,14 @@
-function mostrarSaludo() {
+function mostrarSaludo(event){
+    event.stopPropagation();
     alert('Hola!');
 }
 document.addEventListener("DOMContentLoaded", function() {
     const divContenedor = document.querySelector(".contenedor");
+    const btnSaludar = document.getElementById("btnSaludar");
+
 
     divContenedor.addEventListener("click", function() {
         alert("Hola! Soy el div");
     });
+     btnSaludar.addEventListener("click", mostrarSaludo);
 });
